033f2130e82e9841b5a8d95a6b1dd7fc83fbe4ce
NaveenRayapati <79656203+NaveenRayapati@users.noreply.github.com>
2025-10-30 13:01:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// index.js
-const http = require('http');
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 8080;
+const instanceId = process.env.INSTANCE_ID || 'Unknown Instance';
+const version = 'v1.0.0'; // <-- CHANGE THIS VERSION FOR DEPLOYMENTS!
 
-const requestHandler = (req, res) => {
-  if (req.url === '/healthz') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    return res.end('OK');
-  }
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Node app on ' + (process.env.INSTANCE_ID || 'unknown'));
-};
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Hello from Node App!</h1>
+    <p>Version: ${version}</p>
+    <p>Instance ID: ${instanceId}</p>
+  `);
+});
 
-const server = http.createServer(requestHandler);
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 });
